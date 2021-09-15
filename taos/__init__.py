ad201c664310efec44259f54c928748ad7795d56
Aurélien Ponte <aurelien.junk@gmail.com>
2021-09-15 22:11:36 +0200

    add launch code for ichthyop

--- taos/__init__.py
+++ taos/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ["mars",]
+__all__ = ["mars", "drifters"]
--- taos/drifters.py
+++ taos/drifters.py
@@ -1,5 +1,10 @@
+import os, sys
+import shutil
+
+#from glob import glob
+import time
+
 import xml.etree.ElementTree as ET
-import os
 
 def update_config(file_in, file_out=None, overwrite=False, **params):
     """ Modify an Ichthyop xml input file
@@ -39,3 +44,98 @@ def update_config(file_in, file_out=None, overwrite=False, **params):
         print("File {} has been generated".format(file_out))
     else:
         print("Nothing done")
+
+_default_ichthyop_path = os.path.join(os.getenv("HOME"),
+                                      "ichthyop/target/",
+                                      "ichthyop-3.3.10-jar-with-dependencies.jar",
+                                      )
+
+class ichthy(object):
+    """ Object to automate Ichthyop run launchings
+
+    Parameters
+    ----------
+    rundir: str
+        Name of the run directory
+    jobname: str
+        Name of the PBS job
+    workdir: str, optional
+        Path to the working directory, default to scratch
+    launch: boolean, optional
+        Launch simulation, default is True
+    ichthyop_path: str, optional
+        Path to jar executable
+    """
+
+    def __init__(self,
+                 rundir,
+                 jobname='icht',
+                 workdir=None,
+                 launch=True,
+                 ichthyop_path=None,
+                 **params,
+                 ):
+        #
+        self.startdir = os.getcwd()
+        #
+        if workdir is None:
+            self.workdir = os.getenv('SCRATCH')
+        elif not os.path.isdir(workdir):
+            self.workdir = os.getenv(workdir)
+        else:
+            self.workdir = workdir
+        # main directory where the simulation will be run:
+        self.rpath = os.path.join(self.workdir, rundir)
+        print('Run will be stored in {}'.format(self.rpath))
+        self._create_rpath()
+        # change input parameters
+        self.update_cfg_file(**params)
+        # guess config if necessary and create job files
+        self.jobname = jobname
+        if ichthyop_path is None:
+            ichthyop_path = _default_ichthyop_path
+        self._create_job_files(ichthyop_path)
+        # launch runs
+        if launch:
+            self.launch()
+        os.chdir(self.startdir)
+
+    def _create_rpath(self):
+        if os.path.exists(self.rpath) :
+            os.system('rm -Rf '+self.rpath)
+        os.mkdir(self.rpath)
+        # move to run dir
+        os.chdir(self.rpath)
+
+    def update_cfg_file(self, **params):
+        """ Update config
+        """
+        cfg_in = os.path.join(self.startdir, "taos_mars3d.xml")
+        update_config(cfg_in, file_out="cfg.xml", overwrite=True, **params)
+
+    def _create_job_files(self, ichthyop_path):
+
+        with open('job.pbs','w') as f:
+            f.write('#!/bin/csh\n')
+            f.write('#PBS -N '+self.jobname+'\n')
+            f.write('#PBS -q sequentiel\n')
+            f.write('#PBS -l mem=10g\n')
+            f.write('#PBS -l walltime=01:00:00\n')
+            f.write('\n')
+            f.write('# cd to the directory you submitted your job\n')
+            f.write('cd $PBS_O_WORKDIR\n')
+            f.write('\n')
+            f.write('setenv PATH ${HOME}/.miniconda3/envs/ichthy/bin:${PATH}\n')
+            f.write('\n')
+            f.write('date\n')
+            f.write('java -jar {} cfg.xml\n'.format(ichthyop_path))
+            f.write('\n')
+            f.write('date\n')
+
+    def launch(self):
+        """ Launch simulations
+        """
+        time.sleep(1)
+        #os.chdir(join(self.rpath,'t1'))
+        os.system('qsub job.pbs')
+        os.chdir(self.startdir)
